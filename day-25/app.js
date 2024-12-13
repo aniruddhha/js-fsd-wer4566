26e0ffa2ba881d0a79f0302421323d315983ddf9
aniruddhha <aniruddha.kudalkar@gmail.com>
2024-12-13 15:46:11 +0530

    rest creation and consumption

--- day-25/app.js
+++ day-25/app.js
@@ -1,16 +1,34 @@
 const express = require('express')
 
 const app = express()
-
 const port = 3000
 
+app.use(express.json()) // middleware 
 
-app.post('/', (req, res) => {}) // CREATE
-app.get('/', (req, res) => {}) // READ
-app.put('/',(req, res) => {}) // UPDATE
-app.delete('/', (req, res) => {}) // DELETE
+ // CREATE: POST - http://localhost:3000/create
+app.post('/create', (req, res) => {
+    // when you hit above url, this function body gets executed
 
+    res.json({
+        sts: 'success',
+        msg : 'post method invoked successfully'
+    })
+})
 
+// READ: GET - http://localhost:3000/read
+app.get('/read', (req, res) => {
+    return res.json({ sts: 'success', msg :'Communication Check Successful' })
+}) 
+
+// UPDATE: PUT - http://localhost:3000/update
+app.put('/update',(req, res) => {
+
+}) 
+
+// DELETE: DELETE - http://localhost:3000/delete
+app.delete('/delete', (req, res) => {
+
+}) 
 
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`)

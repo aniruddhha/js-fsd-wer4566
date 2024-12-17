33132e9273a1057d191bd6d13b69a44df7b0e427
aniruddhha <aniruddha.kudalkar@gmail.com>
2024-12-17 15:42:34 +0530

    middleware understanding

--- 27-day/app.js
+++ 27-day/app.js
@@ -4,6 +4,18 @@ const app = express()
 const carRouter = require('./car.routes')
 const showRoomRouter = require('./showroom.routes')
 
+app.use((req, res, next) => { // middleware 
+    next()
+}, (req, res, next) => { // middleware 
+    next()
+})
+
+const cleanReqMiddleware = (req, res, nex) => {
+    next()
+}
+
+app.use(cleanReqMiddleware)
+
 app.use('/car', carRouter)
 app.use('/showroom', showRoomRouter)
 

d2a6e7df62c083e3b6af47174ae19b1d802fda41
aniruddhha <aniruddha.kudalkar@gmail.com>
2024-12-18 15:40:24 +0530

    es6 features: arrow functions

---
+++ 28-day/arrows.js
@@ -0,0 +1,17 @@
+function sqr(x) {
+    return x * x
+}
+
+const sqrr = x => x * x
+
+function makeApiCall() {
+    const url = 'https://google.com'
+
+    fetch(url)
+}
+
+const apiCall = () => {
+    const url = 'https://facebook.com'
+
+    fetch(url)
+}
